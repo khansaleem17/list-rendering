6e012bec36defb09922f45b9d8b95e0913e3a149
Saleem <saleem@capiot.com>
2020-03-12 16:17:04 +0530

    list integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { IPageInfo } from 'ngx-virtual-scroller';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'list-rendering';
+export class AppComponent implements OnInit {
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.getData();
+    console.log('saleem', this.data);
+  }
+
+  getData() {
+    const data = localStorage.getItem('listData');
+    if (data) {
+      this.data = JSON.parse(data);
+      return;
+    }
+    // console.log('data', data);
+    this.http.get('http://jivoxdevuploads.s3.amazonaws.com/eam-dev/files/44939/Rule%20JSON.json').pipe(
+      catchError(e => {
+        return e;
+      }),
+      tap(response => {
+        if (response && response['data']) {
+          const resData = JSON.stringify(response['data']);
+          localStorage.setItem('listData', data);
+        }
+      })
+    ).subscribe();
+  }
+
+  fetchMore(event: IPageInfo) {
+    console.log(event)
+    if (event.endIndex === this.data.length - 1) {
+      console.log('hello');
+    }
+  }
+  trackByFn(index, item) {
+    return index; // or item.id
+  }
+  moveUp(e, i) {
+    console.log(i)
+  }
+  clone(item, i) {
+    this.data.unshift(item)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VirtualScrollerModule } from 'ngx-virtual-scroller';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    VirtualScrollerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
